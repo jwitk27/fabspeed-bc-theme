5c86383f47162265f4efb2303094277df1d22feb
James Witkowski <jamesw@fabspeed.local>
2026-06-12 09:35:51 -0400

    updates

--- assets/js/theme/custom/bulk-export-machine.js
+++ assets/js/theme/custom/bulk-export-machine.js
@@ -5,7 +5,7 @@ export default function bulkExportMachine() {
 
   let file;
 
-  $('#bem-file').on("change", e => {
+  $('#upload-price-list').on("change", e => {
     Papa.parse($(e.currentTarget)[0].files[0], {
       header: true,
       skipEmptyLines: true,
@@ -23,20 +23,57 @@ export default function bulkExportMachine() {
         const noteAdded = addNote(blankSkusRemoved);
 
         const priceListFinalProducts = noteAdded;
-        const bulkExportFinalProducts = blankSkusRemoved;
         // Unparse the .csv file for download
         const priceListCsv = Papa.unparse(priceListFinalProducts);
         const priceListCsvData = new Blob([priceListCsv], { type: 'text/csv;charset=utf-8;' });
         const priceListCsvURL = window.URL.createObjectURL(priceListCsvData);
 
+        $('#download-price-list').show();
+
+        $('#download-price-list').attr('href', priceListCsvURL);
+        $('#download-price-list').attr('download', 'price-sheet.csv');
+
+        $('#download-price-list').on('click', () => {
+          $('#download-price-list').hide();
+          $('#upload-price-list').val('');
+        });
+      },
+      error: (err) => {
+        console.error("[BEM] Papa error:", err);
+      },
+    });
+  });
+
+  $('#upload-bulk-export').on("change", e => {
+    Papa.parse($(e.currentTarget)[0].files[0], {
+      header: true,
+      skipEmptyLines: true,
+      worker: true,
+      complete: (results) => {
+        // Remove Item Type: Rule from product export - only need "products" and "skus"
+        const itemTypeRuleRemoved = removeItemTypeRule(results.data);
+        // Remove products + child skus where product visible value is 0
+        const visibleOnly = showVisibleOnly(itemTypeRuleRemoved);
+        // If exported skus are left blank, make them the same price as parent product (this is how it works in BigCommerce if a sku price is left blank)
+        const pricePopulated = populateMissingPrice(visibleOnly);
+        // Remove blank skus
+        const blankSkusRemoved = removeBlankSkus(pricePopulated);
+
+        const bulkExportFinalProducts = blankSkusRemoved;
+
         const bulkExportCsv = Papa.unparse(bulkExportFinalProducts);
         const bulkExportCsvData = new Blob([bulkExportCsv], { type: 'text/csv;charset=utf-8;' });
         const bulkExportCsvURL = window.URL.createObjectURL(bulkExportCsvData);
 
-        $('.bem-download').show();
+        $('#download-bulk-export').show();
 
-        $('#download-price-list').attr('href', priceListCsvURL);
         $('#download-bulk-export').attr('href', bulkExportCsvURL);
+        $('#download-bulk-export').attr('download', 'bulk-export.csv');
+
+        $('#download-bulk-export').on('click', () => {
+          $('#download-bulk-export').hide();
+          $('#upload-bulk-export').val('');
+        });
       },
       error: (err) => {
         console.error("[BEM] Papa error:", err);
@@ -66,15 +103,21 @@ export default function bulkExportMachine() {
   function showVisibleOnly(rows) {
     rows.forEach((row, i) => {
       let rowVisibility;
+      let visibleIndicator;
+      let invisibleIndicator;
       if (row['Product Visible'] === undefined) {
         rowVisibility = 'Product Visible?'
+        visibleIndicator = 'Y';
+        invisibleIndicator = 'N';
       } else {
         rowVisibility = 'Product Visible'
+        visibleIndicator = '1';
+        invisibleIndicator = '0';
       }
-      if (row['Item Type'].trim() === 'Product' && row[rowVisibility].trim() == '0') {
+      if (row['Item Type'].trim() === 'Product' && row[rowVisibility].trim() == invisibleIndicator) {
         while (true) {
           rows.splice(i, 1);
-          if (rows[i] === undefined || rows[i]['Item Type'].trim() === 'Product' && rows[i][rowVisibility].trim() == '1') {
+          if (rows[i] === undefined || rows[i]['Item Type'].trim() === 'Product' && rows[i][rowVisibility].trim() == visibleIndicator) {
             return;
           }
         }
@@ -84,7 +127,11 @@ export default function bulkExportMachine() {
   }
 
   function removeBlankSkus(rows) {
-    return rows.filter(row => row['Product SKU'].trim() !== '');
+    if (rows[0]['Product SKU'] == undefined) {
+      return rows.filter(row => row['Product Code/SKU'].trim() !== '');
+    } else {
+      return rows.filter(row => row['Product SKU'].trim() !== '');
+    }
   }
 
   function addNote(rows) {
--- assets/js/theme/custom/sound-button.js
+++ assets/js/theme/custom/sound-button.js
@@ -1,24 +1,7 @@
 export default function () {
-    if ($('#playButton').length) {
-        $('.productView-options').before(getPlayButtonClone('desktop'));
-        $('.productView-product').before(getPlayButtonClone('mobile'));
-    }
-
-    function getPlayButtonClone(format) {
-        const $playClone = $('#playButton').parent().clone();
-
-        $playClone.find('#playButton').addClass('playButton');
-        $playClone.find('#productAudio').addClass('productAudio');
-        $playClone.find('#playButton').removeAttr('id');
-        $playClone.find('span').hide();
-        $playClone.css('display', 'block');
-        $playClone.find('button').html('<img style="max-width:20px;padding-bottom:2px;margin-right:2px;" src="https://cdn11.bigcommerce.com/s-fh9wsjv2/images/stencil/original/image-manager/speaker-white-icon.png"> SOUND PREVIEW');
-        $playClone.addClass(`sound-button ${format}`);
-        $playClone.find('.playButton').on('click', function () {
-            const $audio = $playClone.find('.productAudio').get(0);
-            $audio.currentTime = 0;
-            $audio.play();
+    $('.playButton').each((i, el) => {
+        $(el).on('click', () => {
+            $('#productAudio')[0].play();
         });
-        return $playClone;
-    }
+    });
 }
